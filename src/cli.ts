3005dd2f8273d307c6d2ab15bf16d2d2a11cf04b
Sean McLellan <oceanswave@clawdbot.lan>
2026-02-08 08:51:54 -0500

    feat: Add new commands (thread, replies, mentions, etc)

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { whoami, readTweet, searchTweets, listTimeline, postTweet } from './commands.js';
+import {
+  whoami,
+  readTweet,
+  searchTweets,
+  listTimeline,
+  postTweet,
+  thread,
+  replies,
+  mentions,
+  userTweets,
+  home,
+  bookmarks,
+  follow,
+  unfollow,
+  lists,
+  reply,
+  like,
+  retweet,
+} from './commands.js';
 
 const program = new Command();
 
@@ -37,5 +55,74 @@ program
   .description('Post a tweet')
   .argument('<text>', 'Tweet text')
   .action(postTweet);
+
+program
+  .command('thread')
+  .description('Read a tweet thread/conversation')
+  .argument('<urlOrId>', 'Tweet URL or ID')
+  .action(thread);
+
+program
+  .command('replies')
+  .description('Get replies to a tweet')
+  .argument('<urlOrId>', 'Tweet URL or ID')
+  .action(replies);
+
+program
+  .command('mentions')
+  .description('Get your mentions')
+  .action(mentions);
+
+program
+  .command('user-tweets')
+  .description("Get a user's tweets")
+  .argument('<handle>', 'Username (with or without @)')
+  .action(userTweets);
+
+program
+  .command('home')
+  .description('Read your home timeline')
+  .action(home);
+
+program
+  .command('bookmarks')
+  .description('Get your bookmarks')
+  .action(bookmarks);
+
+program
+  .command('follow')
+  .description('Follow a user')
+  .argument('<handle>', 'Username (with or without @)')
+  .action(follow);
+
+program
+  .command('unfollow')
+  .description('Unfollow a user')
+  .argument('<handle>', 'Username (with or without @)')
+  .action(unfollow);
+
+program
+  .command('lists')
+  .description('Get your lists')
+  .action(lists);
+
+program
+  .command('reply')
+  .description('Reply to a tweet')
+  .argument('<urlOrId>', 'Tweet URL or ID')
+  .argument('<text>', 'Reply text')
+  .action(reply);
+
+program
+  .command('like')
+  .description('Like a tweet')
+  .argument('<urlOrId>', 'Tweet URL or ID')
+  .action(like);
+
+program
+  .command('retweet')
+  .description('Retweet/repost a tweet')
+  .argument('<urlOrId>', 'Tweet URL or ID')
+  .action(retweet);
 
 program.parse();
